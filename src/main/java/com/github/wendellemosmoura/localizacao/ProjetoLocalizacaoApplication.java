5f81d225d25436c7dc6202dc00755a5b46cc023c
wendel.lemos <wendel_lemos_cg@hotmail.com>
2022-12-07 14:40:31 -0300

    Initial commit.

---
+++ src/main/java/com/github/wendellemosmoura/localizacao/ProjetoLocalizacaoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wendellemosmoura.localizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLocalizacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoLocalizacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wendellemosmoura/localizacao/ProjetoLocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wendellemosmoura.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLocalizacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
